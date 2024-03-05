7a60a1265f76f33ae1f05818a7739c784bdf5f71
Xiaokang2022 <2951256653@qq.com>
2024-03-05 23:33:10 +0800

    Deployed b44b6d9 with MkDocs version: 1.5.3

---
+++ extra/js/tongji.baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?588b1b60f93b646c65e339138b590c1f";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
