53b9eed052cb851f05d5588702953b81296c90a1
Xiaokang2022 <2951256653@qq.com>
2024-03-07 22:42:35 +0800

    Deployed 52cd4c5 with MkDocs version: 1.5.3

--- extra/js/shortcuts.js
+++ extra/js/shortcuts.js
@@ -1,5 +1,3 @@
-// Author: Xiaokang2022
-
 keyboard$.subscribe(function (key) {  // 键盘左键上一页
     if (key.mode === "global" && key.type === "ArrowLeft") {
         /* Add custom keyboard handler here */
