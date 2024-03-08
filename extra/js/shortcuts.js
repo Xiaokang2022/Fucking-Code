2b8bc2d2d59812148ca28ae9959c49733e4690de
Xiaokang2022 <2951256653@qq.com>
2024-03-08 22:47:20 +0800

    Deployed a43732e with MkDocs version: 1.5.3

--- extra/js/shortcuts.js
+++ extra/js/shortcuts.js
@@ -17,3 +17,23 @@ keyboard$.subscribe(function (key) {  // 键盘右键下一页
         key.claim()
     }
 })
+
+// var flag = false;
+
+// keyboard$.subscribe(function (key) {  // 键盘双击上键回到顶部
+//     function reset() {
+//         flag = false;
+//         clearInterval(inter);
+//     }
+//     if (key.mode === "global" && key.type === "ArrowUp") {
+//         /* Add custom keyboard handler here */
+//         if (!flag) {
+//             var inter = setInterval(reset, 150);
+//             flag = true;
+//             return;
+//         }
+//         var elements = document.getElementsByClassName('md-top md-icon');
+//         if (elements.length)
+//             elements[0].click();
+//     }
+// })
